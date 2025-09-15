f8b2e840a705f2a415108c65ec7e47c1128cd212
Daniel Sandiego <dnlsandiego@gmail.com>
2025-09-15 03:33:00 -0500

    initial commit

---
+++ src/use-sync-external-store-with-selector.ts
@@ -0,0 +1,122 @@
+import {
+  useDebugValue,
+  useEffect,
+  useMemo,
+  useRef,
+  useSyncExternalStore,
+} from 'react'
+
+type Inst<Selection> =
+  | {
+      hasValue: true
+      value: Selection
+    }
+  | {
+      hasValue: false
+      value: null
+    }
+
+export function useSyncExternalStoreWithSelector<Snapshot, Selection>(
+  subscribe: (onStoreChange: () => void) => () => void,
+  getSnapshot: () => Snapshot,
+  getServerSnapshot: void | null | (() => Snapshot),
+  selector: (snapshot: Snapshot) => Selection,
+  isEqual?: (a: Selection, b: Selection) => boolean,
+): Selection {
+  // Use this to track the rendered snapshot.
+  const instRef = useRef<Inst<Selection> | null>(null)
+  let inst: Inst<Selection>
+  if (instRef.current === null) {
+    inst = {
+      hasValue: false,
+      value: null,
+    }
+    instRef.current = inst
+  } else {
+    inst = instRef.current
+  }
+
+  const [getSelection, getServerSelection] = useMemo(() => {
+    // Track the memoized state using closure variables that are local to this
+    // memoized instance of a getSnapshot function. Intentionally not using a
+    // useRef hook, because that state would be shared across all concurrent
+    // copies of the hook/component.
+    let hasMemo = false
+    let memoizedSnapshot: Snapshot
+    let memoizedSelection: Selection
+
+    const memoizedSelector = (nextSnapshot: Snapshot) => {
+      if (!hasMemo) {
+        // The first time the hook is called, there is no memoized result.
+        hasMemo = true
+        memoizedSnapshot = nextSnapshot
+        const nextSelection = selector(nextSnapshot)
+
+        if (isEqual !== undefined) {
+          // Even if the selector has changed, the currently rendered selection
+          // may be equal to the new selection. We should attempt to reuse the
+          // current value if possible, to preserve downstream memoizations.
+          if (inst.hasValue) {
+            const currentSelection = inst.value
+            if (isEqual(currentSelection, nextSelection)) {
+              memoizedSelection = currentSelection
+              return currentSelection
+            }
+          }
+        }
+
+        memoizedSelection = nextSelection
+        return nextSelection
+      }
+
+      const prevSnapshot = memoizedSnapshot
+      const prevSelection = memoizedSelection
+
+      if (Object.is(prevSnapshot, nextSnapshot)) {
+        // The snapshot is the same as last time. Reuse the previous selection.
+        return prevSelection
+      }
+
+      // The snapshot has changed, so we need to compute a new selection.
+      const nextSelection = selector(nextSnapshot)
+
+      // If a custom isEqual function is provided, use that to check if the data
+      // has changed. If it hasn't, return the previous selection. That signals
+      // to React that the selections are conceptually equal, and we can bail
+      // out of rendering.
+      if (isEqual !== undefined && isEqual(prevSelection, nextSelection)) {
+        // The snapshot still has changed, so make sure to update to not keep
+        // old references alive
+        memoizedSnapshot = nextSnapshot
+        return prevSelection
+      }
+
+      memoizedSnapshot = nextSnapshot
+      memoizedSelection = nextSelection
+      return nextSelection
+    }
+
+    const getSnapshotWithSelector = () => memoizedSelector(getSnapshot())
+    const getServerSnapshotWithSelector =
+      typeof getServerSnapshot === 'function'
+        ? () => memoizedSelector(getServerSnapshot())
+        : undefined
+
+    return [getSnapshotWithSelector, getServerSnapshotWithSelector]
+  }, [getSnapshot, getServerSnapshot, selector, isEqual])
+
+  const value = useSyncExternalStore(
+    subscribe,
+    getSelection,
+    getServerSelection,
+  )
+
+  useEffect(() => {
+    inst.hasValue = true
+    inst.value = value
+  }, [value])
+
+  useDebugValue(value)
+
+  return value
+}
